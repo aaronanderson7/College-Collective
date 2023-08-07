3c7b750713eeb65a8c47e457d9df2916a5097abe
Aaron Anderson <anderaa5@oregonstate.edu>
2023-08-07 14:30:23 -0700

    Second commit

---
+++ app.js
@@ -0,0 +1,204 @@
+// App.js
+
+
+// Database
+var db = require('./database/db-connector');
+
+/*
+    SETUP
+*/
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+
+PORT        = 4779;                 // Set a port number at the top so it's easy to change in the future
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+/*
+    ROUTES
+*/
+
+
+/*
+    SEARCH opeartions for classes.
+*/
+app.get('/', function(req, res)
+{
+    // Declare Query 1
+    let query1;
+
+    // If there is no query string, we just perform a basic SELECT
+    if (req.query.className === undefined)
+    {
+        query1 = "SELECT * FROM Classes;";
+    }
+
+    // If there is a query string, we assume this is a search, and return desired results
+    else
+    {
+        query1 = `SELECT * FROM Classes WHERE className LIKE "${req.query.className}%"`
+    }
+
+    // Query 2 is the same in both cases
+    let query2 = "SELECT * FROM Departments;";
+
+    // Run the 1st query
+    db.pool.query(query1, function(error, rows, fiels){
+
+        // Save the classes
+        let classes = rows
+
+        // Run the second query
+        db.pool.query(query2, (error, rows, fields) => {
+
+            // Save the departments
+            let departments = rows;
+            return res.render('index', {data: classes, departments: departments})
+        })
+    })
+});
+
+
+/*
+    INSERT Operations for Classes
+*/
+
+app.post('/add-class-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Capture NULL values
+        let departmentID = parseInt(data.departmentID);
+        if (isNaN(departmentID))
+        {
+            className = 'NULL'
+        }
+    
+        let professorID = parseInt(data.professorID);
+        if (isNaN(professorID))
+        {
+            credit = 'NULL'
+        }
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Classes (className, credit, departmentID, professorID) VALUES ('${data.className}', '${data.credit}', ${departmentID}, ${professorID})`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Classes;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+
+/*
+    DELETE OPERATION for Classes
+*/
+
+app.delete('/delete-class-ajax/', function(req,res,next){
+    let data = req.body;
+    let classID = parseInt(data.id);
+    let deleteStudents_has_Classes = `DELETE FROM Students_has_Classes WHERE classID = ?`;
+    let deleteClasses = `DELETE FROM Classes where classID = ?`;
+  
+  
+          // Run the 1st query
+          db.pool.query(deleteStudents_has_Classes, [classID], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
+              else
+              {
+                  // Run the second query
+                  db.pool.query(deleteClasses, [classID], function(error, rows, fields) {
+  
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.sendStatus(204);
+                      }
+                  })
+              }
+  })});
+
+/*
+  UPDATE OPERATION for Classes
+*/
+
+app.put('/put-class-ajax', function(req,res,next){
+    let data = req.body;
+  
+    let professor = parseInt(data.professor);
+    let classes = parseInt(data.className);
+  
+    let queryUpdateProfessor = `UPDATE Classes SET professor = ? WHERE Classes.id = ?`;
+    let selectProfessor = `SELECT * FROM Professors WHERE id = ?`;
+  
+          // Run the 1st query
+          db.pool.query(queryUpdateProfessor, [professor, classes], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
+              // If there was no error, we run our second query and return that data so we can use it to update the people's
+              // table on the front-end
+              else
+              {
+                  // Run the second query
+                  db.pool.query(selectProfessor, [professor], function(error, rows, fields) {
+  
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.send(rows);
+                      }
+                  })
+              }
+  })});
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ public/js/add_class.js
@@ -0,0 +1,120 @@
+// Get the objects we need to modify
+let addClassForm = document.getElementById('add-class-form-ajax');
+
+// Modify the objects we need
+addClassForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputClassName = document.getElementById("input-className");
+    let inputCredit = document.getElementById("input-credit");
+    let inputdepartmentID= document.getElementById("input-departmentID-ajax");
+    let inputprofessorID = document.getElementById("input-professorID");
+
+    // Get the values from the form fields
+    let classNameValue = inputClassName.value;
+    let creditValue = inputCredit.value;
+    let departmentIDValue = inputdepartmentID.value;
+    let professorIDValue = inputprofessorID.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        className: classNameValue,
+        credit: creditValue,
+        departmentID: departmentIDValue,
+        professorID: professorIDValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-class-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputClassName.value = '';
+            inputCredit.value = '';
+            inputdepartmentID.value = '';
+            inputprofessorID.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("class-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let classIDCell = document.createElement("TD");
+    let classNameCell = document.createElement("TD");
+    let creditCell = document.createElement("TD");
+    let departmentIDCell = document.createElement("TD");
+    let professorIDCell = document.createElement("TD");
+
+    let deleteCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    classIDCell.innerText = newRow.classID;
+    classNameCell.innerText = newRow.className;
+    creditCell.innerText = newRow.credit;
+    departmentIDCell.innerText = newRow.departmentID;
+    professorIDCell.innerText = newRow.professorID;
+
+    deleteCell = document.createElement("button");
+    deleteCell.innerHTML = "Delete";
+    deleteCell.onclick = function(){
+        deleteClass(newRow.id);
+    };
+
+    // Add the cells to the row 
+    row.appendChild(classIDCell);
+    row.appendChild(classNameCell);
+    row.appendChild(creditCell);
+    row.appendChild(departmentIDCell);
+    row.appendChild(professorIDCell);
+    
+
+    // Add a row attribute so the deleteRow function can find a newly added row. 
+    row.setAttribute('data-value', newRow.id);
+
+    // Add the row to the table
+    currentTable.appendChild(row);
+
+    // Start of new STEP 8 code for adding new data to the dropdown menu for updating class professor. 
+    // Find drop down menu, create a new option, fill data in the option (full name, id),
+    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+    let selectMenu = document.getElementById("mySelect");
+    let option = document.createElement("option");
+    option.text = newRow.className;
+    option.value = newRow.classID;
+    selectMenu.add(option);
+    // End of new step 8 code.
+
+}
