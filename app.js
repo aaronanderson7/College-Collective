09383cb51b052af4b4541f022b02eb24fd8fa3a2
Steve Rector <steverector27@gmail.com>
2023-08-13 12:03:57 -0700

    some add operation for professors done, dropdowns not working

--- app.js
+++ app.js
@@ -389,6 +389,48 @@ app.get('/professors', function(req, res){
 });
 
 /*
+    INSERT Operation 
+*/
+app.post('/add-professor-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Professors (lastName, email, departmentID) VALUES ('${data.professorLastName}, ${data.email}, ${data.departmentID}')`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on Departments
+                query2 = `SELECT * FROM Professors;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+
+/*
 ----------------------------
 StudentsHasProfessors ROUTES 
 ----------------------------
